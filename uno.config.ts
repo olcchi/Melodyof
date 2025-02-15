fd29f85209a6bca6ce85943973b1e3dbdb52e33e
ekar <earthb0y@outlook.com>
2025-02-15 15:51:10 +0800

    initial commit

---
+++ uno.config.ts
@@ -0,0 +1,13 @@
+import presetUno from '@unocss/preset-uno'
+import presetWebFonts from '@unocss/preset-web-fonts'
+import { defineConfig } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetWebFonts({ /* options */
+
+    }),
+
+  ],
+})
